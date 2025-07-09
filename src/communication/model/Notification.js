9a66df04ac26c164637fc3a957d365458edc2a02
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-07-09 08:11:55 -0500

    feat : add comments Notification.js

--- src/communication/model/Notification.js
+++ src/communication/model/Notification.js
@@ -1,13 +1,18 @@
 // src/communication/model/Notification.js
+
+// Define the Notification class
 export default class Notification {
+    
+    // Constructor to initialize a new Notification instance with default or provided values
     constructor(
-        typesNotificationsId = 0,
-        ownersId = 0,
-        adminsId = 0,
-        workersId = 0,
-        title = "",
-        description = ""
+        typesNotificationsId = 0,  // ID of the notification type (default: 0)
+        ownersId = 0,  // ID of the owner (default: 0)
+        adminsId = 0,  // ID of the admin (default: 0)
+        workersId = 0,  // ID of the worker (default: 0)
+        title = "",  // Title of the notification (default: empty string)
+        description = ""  // Description of the notification (default: empty string)
     ) {
+        // Assign the provided or default values to the instance properties
         this.typesNotificationsId = typesNotificationsId;
         this.ownersId = ownersId;
         this.adminsId = adminsId;
@@ -16,25 +21,27 @@ export default class Notification {
         this.description = description;
     }
 
+    // Static method to create a Notification instance from a JSON object
     static fromJson(json) {
         return new Notification(
-            json.typesNotificationsId,
-            json.ownersId,
-            json.adminsId,
-            json.workersId,
-            json.title,
-            json.description
+            json.typesNotificationsId,  // Extract notification type ID from the JSON
+            json.ownersId,  // Extract owner ID from the JSON
+            json.adminsId,  // Extract admin ID from the JSON
+            json.workersId,  // Extract worker ID from the JSON
+            json.title,  // Extract title from the JSON
+            json.description  // Extract description from the JSON
         );
     }
 
+    // Method to convert the Notification instance to a JSON object
     toJson() {
         return {
-            typesNotificationsId: this.typesNotificationsId,
-            ownersId: this.ownersId,
-            adminsId: this.adminsId,
-            workersId: this.workersId,
-            title: this.title,
-            description: this.description
+            typesNotificationsId: this.typesNotificationsId,  // Map notification type ID to JSON
+            ownersId: this.ownersId,  // Map owner ID to JSON
+            adminsId: this.adminsId,  // Map admin ID to JSON
+            workersId: this.workersId,  // Map worker ID to JSON
+            title: this.title,  // Map title to JSON
+            description: this.description  // Map description to JSON
         };
     }
 }
